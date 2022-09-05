67276430b26790e408389b320156bb0e3d637237
Paul Reiter <reiter.paul@gmail.com>
2022-09-05 12:24:46 +0200

    Add a simple Tomato planet

--- run_game.py
+++ run_game.py
@@ -8,8 +8,28 @@ BACKGROUND_COLOR = (204, 255, 255)  # TODO: use gradient from blue to black
 FPS = 60
 
 
+class Planet:
+    def __init__(self, radius, color, horizon_height):
+        self.radius = radius
+        self.color = color
+        self.horizon_height = horizon_height
+
+    def draw(self, win):
+        rect = pygame.Rect(
+            -self.radius + win.get_width() / 2,
+            win.get_height() - self.horizon_height,
+            2 * self.radius,
+            2 * self.radius,
+        )
+        pygame.draw.ellipse(win, self.color, rect)
+
+
+PLANET = Planet(4000, (255, 0, 0), 200)
+
+
 def draw_window():
     WIN.fill(BACKGROUND_COLOR)
+    PLANET.draw(WIN)
     pygame.display.update()
 
 
