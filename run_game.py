4311c64c792c2e0de5b3d9692b6afcc49f373ad8
Paul Reiter <reiter.paul@gmail.com>
2022-09-05 10:39:44 +0200

    Set window title and background color

--- run_game.py
+++ run_game.py
@@ -2,6 +2,9 @@ import pygame
 
 WIDTH, HEIGHT = 1920, 1080
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Red Planted")
+
+BACKGROUND_COLOR = (204, 255, 255)  # TODO: use gradient from blue to black
 
 
 def main():
@@ -10,6 +13,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+        WIN.fill(BACKGROUND_COLOR)
+        pygame.display.update()
     pygame.quit()
 
 
