4bef9d0cea61a25be35352872ad5fcf869e8a0e6
Paul Reiter <reiter.paul@gmail.com>
2022-09-05 10:52:33 +0200

    Set framerate to 60 FPS

--- run_game.py
+++ run_game.py
@@ -5,11 +5,14 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Red Planted")
 
 BACKGROUND_COLOR = (204, 255, 255)  # TODO: use gradient from blue to black
+FPS = 60
 
 
 def main():
+    clock = pygame.time.Clock()
     run = True
     while run:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
