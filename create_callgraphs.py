d340a276c8056e96d1512ac9241ca695c14a535f
Claus Aichinger <claus.aichinger@gmail.com>
2022-10-03 13:19:21 +0200

    Add script to create call graphs

---
+++ create_callgraphs.py
@@ -0,0 +1,8 @@
+# pip install code2flow
+import subprocess
+
+
+depths = range(5)
+
+for depth in depths:
+    subprocess.run(['code2flow', 'run_game.py', f'--downstream-depth {depth}', '--target-function main', f'--output downstream_depth_{depth}.png'])
